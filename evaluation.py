eff1dc41269ab42c453d5e983873a12d877ed51a
Kaspar <kaspar.daemen@hotmail.com>
2019-12-21 17:04:40 +0100

    t-test comparison added

--- evaluation.py
+++ evaluation.py
@@ -5,9 +5,11 @@ import pytrec_eval
 import json
 import pandas as pd
 import numpy as np
+import scipy.stats
 
 
 # FUNCTIONS:
+
 def reformat_dict(d, qrel):
     result = {}
     for key in d.keys():
@@ -35,16 +37,26 @@ def make_run_dict(file_path):
         d[i] = [{df['document_id'][j]: df['score'][j]} for j in df[df['query_id']==i].index]
     return d
 
-def agg_dict(result_dict):
-    result = {}
-    for key, value in result_dict.items():
-        maps_results = []
-        ndcg_results = []
-        for k, v in value.items():
-            maps_results.append(v['map'])
-            ndcg_results.append(v['ndcg'])
-        result[key] = {'maps': maps_results, 'ndcgs': ndcg_results}
-    return result
+def compare_results(result_dict, result1, result2, measure):
+    if not (measure in ['map','ndcg']):
+        print("invalid measure")
+        return
+    
+    query_ids = [key for key in result_dict[result1].keys()]
+    first_results = result_dict[result1]
+    second_results = result_dict[result2]
+    
+    first_scores = [ first_results[query_id][measure] for query_id in query_ids]
+    second_scores = [second_results[query_id][measure] for query_id in query_ids]
+    
+    print("-----------------------------------------------------------------------------------------")
+    print("Comparison with !{}! as measure: {} VS {} ".format(measure, result1,result2))
+    print("\nThe average score of {} is: {}".format(result1, np.mean(first_scores)))
+    print("The average score of {} is: {}".format(result2, np.mean(second_scores)))
+ 
+    ttest = scipy.stats.ttest_rel(first_scores, second_scores)
+    print("\nT-test results: \nNull hypothesis: 2 related or repeated samples have identical average (expected) values. \nTwo-sided p-value: {} \nT-statistic: {}".format(ttest.pvalue, ttest.statistic))
+    print("-----------------------------------------------------------------------------------------")
     
 
 #EVALUATION
@@ -60,16 +72,11 @@ evaluator = pytrec_eval.RelevanceEvaluator(qrel, {'map', 'ndcg'})
 results = evaluator.evaluate(run)
 results_80 = evaluator.evaluate(run_80)
 
-#with open('queries_expanded_80.json') as f:
-#  test= json.load(f)
 
+#COMPARISON
 result_dict = {
         'results': results,
         'results_80': results_80
         }
-per_result = agg_dict(result_dict)
 
-print('MAP average of results {}'.format(np.mean(per_result['results']['maps'])))
-print('MAP average of results_80 {}'.format(np.mean(per_result['results_80']['maps'])))
-print('gain average of results {}'.format(np.mean(per_result['results']['ndcgs'])))
-print('gain average of results_80 {}'.format(np.mean(per_result['results_80']['ndcgs'])))
+compare_results(result_dict, 'results', 'results_80', 'map')
