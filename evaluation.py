9b477a80a75cf458dad34d5b6142c26fae6e056f
Kaspar <kaspar.daemen@hotmail.com>
2019-12-21 21:25:02 +0100

    evaluation for the new query files

--- evaluation.py
+++ evaluation.py
@@ -2,7 +2,6 @@ import sys
 sys.path.insert(0, "/home/kaspar/Github/nordlys")
 
 import pytrec_eval
-import json
 import pandas as pd
 import numpy as np
 import scipy.stats
@@ -55,28 +54,52 @@ def compare_results(result_dict, result1, result2, measure):
     print("The average score of {} is: {}".format(result2, np.mean(second_scores)))
  
     ttest = scipy.stats.ttest_rel(first_scores, second_scores)
-    print("\nT-test results: \nNull hypothesis: 2 related or repeated samples have identical average (expected) values. \nTwo-sided p-value: {} \nT-statistic: {}".format(ttest.pvalue, ttest.statistic))
+    print("\nT-test results: \nTwo-sided p-value: {} \nT-statistic: {}".format(ttest.pvalue, ttest.statistic))
     print("-----------------------------------------------------------------------------------------")
+def return_all_means(result_dict):
+    query_ids = [key for key in result_dict[next(iter(result_dict))].keys()]
+    means = {}
+    for key in result_dict.keys():
+        mean = np.mean([ result_dict[key][query_id]['map'] for query_id in query_ids])
+        means[key] = mean
+    return means
+    
+    
     
 
 #EVALUATION
 read_qrels = make_qrel_dict('qrels-v2.txt')
-read_run = make_run_dict('output.txt')
-read_run_expanded = make_run_dict('output_expanded_80.txt')  
+read_run = make_run_dict('outputs/output.txt')
+read_run_80 = make_run_dict('outputs/output_expanded_80.txt')
+read_run_90 = make_run_dict('outputs/output_expanded_90.txt')
+read_run_long3_80 = make_run_dict('outputs/output_expanded_long3_80.txt') 
+read_run_long3_90 = make_run_dict('outputs/output_expanded_longer3_90.txt')  
     
 qrel = reformat_dict(read_qrels, True)
 run = reformat_dict(read_run, False)
-run_80 = reformat_dict(read_run_expanded, False)
+run_80 = reformat_dict(read_run_80, False)
+run_90 = reformat_dict(read_run_90, False)
+run_long3_80 = reformat_dict(read_run_long3_80, False)
+run_long3_90 = reformat_dict(read_run_long3_90, False) 
     
 evaluator = pytrec_eval.RelevanceEvaluator(qrel, {'map', 'ndcg'})
 results = evaluator.evaluate(run)
 results_80 = evaluator.evaluate(run_80)
+results_90 = evaluator.evaluate(run_90)
+results_long3_80 = evaluator.evaluate(run_long3_80)
+results_long3_90 = evaluator.evaluate(run_long3_90)
 
 
 #COMPARISON
 result_dict = {
         'results': results,
-        'results_80': results_80
+        'results_80': results_80,
+        'results_90': results_90,
+        'results_long3_80': results_long3_80,
+         'results_long3_90': results_long3_90
         }
 
-compare_results(result_dict, 'results', 'results_80', 'map')
+#compare_results(result_dict, 'results', 'results_90', 'map')
+#compare_results(result_dict, 'results', 'results_90', 'ndcg')
+
+means = return_all_means(result_dict)
--- scoring.py
+++ scoring.py
@@ -13,8 +13,8 @@ config = {"index_name": "dbpedia_2015_10",
   "smoothing_method": "dirichlet",
   "smoothing_param": 2000,
   "fields": ["names", "categories", "attributes", "similar_entity_names", "related_entity_names"],
-  "query_file": "/home/kaspar/Github/information-retrieval/queries_expanded_80.json",
-  "output_file": "/home/kaspar/Github/information-retrieval/output_expanded_80.txt",
+  "query_file": "/home/kaspar/Github/information-retrieval/expanded_queries/queries_expanded_longer3_90.json",
+  "output_file": "/home/kaspar/Github/information-retrieval/outputs/output_expanded_longer3_90.txt",
   "run_id": "bm25"
 }
   
