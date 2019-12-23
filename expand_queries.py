6b738da354d2c6f5908612b86c5e612d80957434
Carlo Geertse <carlogeertse@hotmail.com>
2019-12-23 19:49:17 +0100

    Add notebook for query expansion

--- expand_queries.py
+++
@@ -1,7 +0,0 @@
-from gensim.models import KeyedVectors
-
-# Load Google's pre-trained Word2Vec model.
-word_vectors = KeyedVectors.load_word2vec_format("./data/GoogleNews-vectors-negative300.bin", binary=True)
-result = word_vectors.similar_by_word("cat")
-print(result)
-# print("{}: {:.4f}".format(*result[0]))
